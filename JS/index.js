c18c5b0642ad44baa1f41e511628f25b1a194c15
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-03-07 15:43:03 +0600

    donate now button functionality added

--- JS/index.js
+++ JS/index.js
@@ -2,13 +2,22 @@ function handleDonate(data) {
     const donationAmount = parseFloat(data.previousElementSibling.children[0].value)
     data.previousElementSibling.children[0].value = "";
     if (donationAmount <= 0 || typeof donationAmount === "string" || isNaN(donationAmount) === true) {
-        return alert("Give valid amount")
+        return my_modal_1.showModal()
     }
     else {
-        const donated = data.parentElement.children[0].children[1].children[0];
-        const donatedAmount = parseFloat(donated.innerText)
-        const totalDonation = donationAmount + donatedAmount;
-        donated.innerText = totalDonation;
+        const balance = document.getElementById("donation-balance")
+        const donationBalance = parseFloat(balance.innerText);
+        if (donationAmount > donationBalance) {
+            return my_modal_2.showModal();
+        }
+        else {
+            const donated = data.parentElement.children[0].children[1].children[0];
+            const donatedAmount = parseFloat(donated.innerText)
+            const totalDonation = donationAmount + donatedAmount;
+            donated.innerText = totalDonation;
+            const presentBalance = donationBalance - donationAmount;
+            balance.innerText = presentBalance;
+        }
 
     }
 }
